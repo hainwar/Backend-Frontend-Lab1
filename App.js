376ff05a087a386b180ce5cb5fb232d90deb23ed
HAIRUL ANWAR <136827134+hainwar@users.noreply.github.com>
2024-06-12 15:08:57 +0000

    Button Component

--- App.js
+++ App.js
@@ -1,53 +1,84 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';``
+import { StyleSheet, Text, View, Image } from 'react-native';``
 
 const App = () => {
-  return (
-    <View style={{flex: 1}}>
+  const ButtonCostom = ({ color, text}) => {
+    return (
       <View style={{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems : 'flex-end',
+        backgroundColor: color,
+        width: 90,
+        height: 50,
+        borderRadius: 20,
+        justifyContent: 'center'
       }}>
-        <View style={{
-          backgroundColor: 'red',
-          height: 100,
-          width: 150,
-          justifyContent: 'center',
-          borderRadius: 10,
-          marginBottom: 40,
-        }}>
-          <Text style={{
-            fontSize: 30,
-            color: 'white',
-            textAlign: 'center',
-          }}>
-            Login
-          </Text>
-          
+        <Text style={{
+          textAlign: 'center',
+          color: 'white',
+          fontSize:20
+        }}> {text}
+
+        </Text>
         </View>
-        <View style={{
-          backgroundColor: 'blue',
-          height: 100,
-          width: 150,
-          justifyContent: 'center',
-          borderRadius: 10,
-          marginLeft: 10,
-          marginBottom: 40,
-          
+    )
+  }
+
+  return (
+    <View style={{
+      flex:1,
+      backgroundColor: 'black'
+    }}>
+      <View>
+        <Text style={{
+          fontSize:30,
+          textAlign:'center',
+          top:20,
+          bottom:0,
+          justifyContent:'center',
+          color:'red'
         }}>
-          <Text style={{
-            fontSize: 30,
-            color: 'white',
-            textAlign: 'center',
-          }}>
-            Register
-          </Text>
+          Selamat Datang di Welcome
+        </Text>
+        </View>
+
+        <View style={{
+          flex:1,
+          alignItems:'center',
+          justifyContent:'center',
+          top:200
+        }}>
+          <Image source={require('./assets/bg.jpg')} style={{
+            width:300,
+            height:300,
+            resizeMode:'contain'
+          }} />
+        </View>
+
+        <View style={{
+          flex:1,
+          alignItems:'flex-end',
+          justifyContent:'center',
+          flexDirection: 'row',
+          justifyContent: 'spacy-evenly',
+          marginBottom:100
+        }}>
+          <View style={{
+            flex:1,
+            width: 80,
+            height:40,
+            justifyContent:'center',
+            borderRadius:25,
+            flexDirection:'row',
+            columnGap:20
+            }}>
+              <ButtonCostom color='red' text='Login'/>
+              <ButtonCostom color='red' text='Sign Up'/>
           </View>
-      </View>
+
+        </View>
+
     </View>
-  );
+  )
+  
 }
 
 export default App;
