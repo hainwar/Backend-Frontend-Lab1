a625aa8af6e89103f96e9234291021aae68a2866
HAIRUL ANWAR <136827134+hainwar@users.noreply.github.com>
2024-07-12 01:39:41 +0000

    .

--- assets/components/profilePage/MyOrders.js
+++ assets/components/profilePage/MyOrders.js
@@ -13,14 +13,49 @@ const MyOrders = () => {
 
 const orders = {
     delivered: [
-        { id: 1, name: 'Order 1', date: '12/12/2021', price: 100 },
-        { id: 2, name: 'Order 2', date: '12/12/2021', price: 200 },
+        { 
+            id: 1, 
+            name: 'Order No.41109121', 
+            date: '12/12/2021', 
+            price: 100,
+            tracNum : '41109121',
+            quantity : 5,
+            status : 'Delivered',
+        },
+        { 
+            id: 2, 
+            name: 'Order No.45643445', 
+            date: '12/12/2021', 
+            price: 500,
+            noOrder : '5841096861',
+            quantity : 6,
+            status : 'Delivered',
+        },
+        { 
+            id: 3,
+            name: 'Order No.546447345', 
+            date: '12/12/2021', 
+            price: 80,
+            noOrder : '5841109791',
+            quantity : 9,
+            status : 'Delivered',
+        },
+        { 
+            id: 4, 
+            name: 'Order No.58589121', 
+            date: '12/12/2021', 
+            price: 100,
+            noOrder : '58110621',
+            quantity : 7,
+            status : 'Delivered',
+        },
+
     ],
     processing: [
-        { id: 4, name: 'Order 4', date: '12/12/2021', price: 400 },
+        { id: 4, name: 'Order No.5841109221', date: '12/12/2021', price: 400 },
     ],
     cancelled: [
-        { id: 7, name: 'Order 7', date: '12/12/2021', price: 700 },
+        { id: 7, name: 'Order No.5841109123', date: '12/12/2021', price: 700 },
     ]
 }
 
@@ -29,53 +64,64 @@ const displayOrders = orders[selectedCategory];
 return (
     <ScrollView style={{
         flex:1,
-        padding:15,
+        padding:20,
         backgroundColor:'F5F5F5',
     }}>
         <Text style={{
-            fontSize:26,
-            padding:15,
+            fontSize:28,
+            padding:5,
             fontFamily:'MetroBold',
+            marginBottom:20,
         }}>My Orders</Text>
 
         <View style={{
             flexDirection:'row',
-            marginBottom:15,
+            marginBottom:20,
             justifyContent:'space-between',
+
         }}>
             <TouchableOpacity style={{
                 padding:10,
-                backgroundColor:selectedCategory === 'delivered' ? 'blue' : 'white',
-                borderRadius:5,
+                backgroundColor:selectedCategory === 'delivered' ? '#222222' : '#F8F8FF',
+                borderRadius:20,
+                width:'30%',
             }}
             onPress={() => setSelectedCategory('delivered')}>
                 <Text style={{
                     fontSize:16,
-                    fontFamily:'MetroBold',
-                }}>Deliverd</Text>
+                    fontFamily:'MetroLight',
+                    textAlign:'center',
+                    color:selectedCategory === 'delivered' ? 'white' : 'black',
+                }}>Delivered</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={{
                 padding:10,
-                backgroundColor:selectedCategory === 'processing' ? 'blue' : 'white',
-                borderRadius:5,
+                backgroundColor:selectedCategory === 'processing' ? '#222222' : '#F8F8FF',
+                borderRadius:20,
+                width:'30%',
             }}
             onPress={() => setSelectedCategory('processing')}>
                 <Text style={{
                     fontSize:16,
-                    fontFamily:'MetroBold',
+                    textAlign:'center',
+                    fontFamily:'MetroLight',
+                    color:selectedCategory === 'processing' ? 'white' : 'black',
                 }}>Processing</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={{
                 padding:10,
-                backgroundColor:selectedCategory === 'cancelled' ? 'blue' : 'white',
-                borderRadius:5,
+                backgroundColor:selectedCategory === 'cancelled' ? '#222222' : '#F8F8FF',
+                borderRadius:20,
+                width:'30%',
             }}
             onPress={() => setSelectedCategory('cancelled')}>
                 <Text style={{
                     fontSize:16,
-                    fontFamily:'MetroBold',
+                    textAlign:'center',
+                    fontFamily:'MetroLight',
+                    color:selectedCategory === 'cancelled' ? 'white' : 'black',
                 }}>Cancelled</Text>
             </TouchableOpacity>
         </View>
@@ -87,29 +133,78 @@ return (
                     backgroundColor: 'white',
                     borderRadius: 5,
                     marginVertical: 5,
-                    flexDirection: 'row',
+                    flexDirection: 'column',
                     justifyContent: 'space-between',
-                    alignItems: 'center',
+                    alignItems: 'flex-start',
                 }}>
                     <Text style={{
-                        fontSize: 16,
-                        fontFamily: 'MetroBold',
+                        fontSize: 15,
+                        fontFamily: 'MetroMedium',
                     }}>
                         {order.name}
                     </Text>
                     <Text style={{
-                        fontSize: 16,
-                        fontFamily: 'MetroBold',
+                        fontSize: 14,
+                        fontFamily: 'MetroLight',
+                        alignSelf:'flex-end',
                     }}>
                         {order.date}
                     </Text>
+                    <Text style={{
+                        fontSize: 14,
+                        fontFamily: 'MetroLight',
+                        alignSelf:'flex-end',
+                        top: 40,
+                    }}>
+                        Total Amount:  <Text style={{fontFamily:'MetroBold'}}>{order.price}$</Text>
+                    </Text>
+                    <Text style={{
+                        fontSize: 14,
+                        fontFamily: 'MetroLight',
+                    }}>
+                       Tracking Number {order.tracNum}
+                    </Text>
+                    <Text style={{
+                        fontSize: 14,
+                        fontFamily: 'MetroLight',
+                        marginTop: 10,
+                    }}>
+                        Quantity: {order.quantity}
+                    </Text>
+                    <Text style={{
+                        fontSize: 14,
+                        alignSelf: 'flex-end',
+                        top:35,
+                        fontFamily: 'MetroLight',
+                        color: order.status === 'Delivered' ? 'green' : 'red',
+                    }}>
+                        {order.status}
+                    </Text>
+
+                    <TouchableOpacity style={{
+                        padding: 10,
+                        borderWidth: 1,
+                        color: 'gray',
+                        borderRadius: 20,
+                        marginTop: 10,
+                        width: '20%',
+                    }}>
+                        <Text style={{
+                            fontSize: 14,
+                            fontFamily: 'MetroMedium',
+                            color: 'gray',
+                            textAlign: 'center',
+                        }}>
+                            Details
+                        </Text>
+                    </TouchableOpacity>
                 </View>
         ))
     ) : (
 
         <Text style={{
             fontSize: 16,
-            fontFamily: 'MetroBold',
+            fontFamily: 'MetroMedium',
             textAlign: 'center',
             marginTop: 10,
         }}>
